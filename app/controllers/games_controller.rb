14f9b39684f783528a907317e82f316dcd055146
Matt Farmer <mrfarmer777@gmail.com>
2018-08-03 04:33:29 +0000

    Done.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,28 @@
+require 'pry'
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    my_state=params[:state]
+    @game=Game.create(state: my_state)
+    render json: @game
+  end
+
+  def show
+    @game=Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+    @game=Game.find(params[:id])
+    @game.update(state: params[:state])
+    render json: @game
+  end
+
+  def index
+    @games=Game.all
+    render json: @games
+  end
+
+  private
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+  post '/games', to: 'games#create'
+  get '/games/:id', to: 'games#show'
+  patch '/games/:id', to: 'games#update'
+  get '/games', to: 'games#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
